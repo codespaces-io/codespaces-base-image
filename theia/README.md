3f00edb4dd5a9c71e023def61d8d7e60b9e9bd0f
Gourav Shah <gs@initcron.org>
2019-02-01 06:38:51 +0000

    added theia build

---
+++ theia/README.md
@@ -0,0 +1,3 @@
+# READ THIS CAREFULLY
+
+This dockerfile is used to create `akurath:v0.1.0` and this image has docker 1.12 installed in it. For the latest image use the `v2` directory.
---
+++ theia/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+
+yarn theia start /workspace --hostname 0.0.0.0 --port 8000
+
+#exec "$@"
+
+
